d800a7d5b26da9631d5be0b7654b1444b8df4f7d
Gunish Katar <gn894984@dal.ca>
2023-11-30 16:59:02 -0400

    Made projects page and added code for removing border during resizing.

---
+++ index.js
@@ -0,0 +1,15 @@
+jQuery(document).ready(function($) {
+    var alterClass = function() {
+      var ww = document.body.clientWidth;
+      if (ww < 600) {
+        $('header').removeClass('border-bottom');
+      } else if (ww >= 601) {
+        $('header').addClass('border-bottom');
+      };
+    };
+    $(window).resize(function(){
+      alterClass();
+    });
+    //Fire it when the page first loads:
+    alterClass();
+  });
