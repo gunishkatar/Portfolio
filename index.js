2db67bb5fab40b93ca8a4192f171795e6cb94cf1
Gunish Katar <gn894984@dal.ca>
2023-12-02 02:13:07 -0400

    Solved the issue of overlapping footer and header. Also, It is a working code and but not a clean code.

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ jQuery(document).ready(function($) {
     var alterClass = function() {
       var ww = document.body.clientWidth;
       if (ww < 600) {
-        $('header').removeClass('border-bottom');
+        $('header').addClass('border-bottom');
+        // $('header').removeClass('border-bottom');
       } else if (ww >= 601) {
         $('header').addClass('border-bottom');
       };
